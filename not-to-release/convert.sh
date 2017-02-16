a7ddc99a03789828738286841b80d8feb7412f61
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-16 23:48:15 +0100

    script for removing ManualCheck=Yes

--- not-to-release/convert.sh
+++ not-to-release/convert.sh
@@ -4,7 +4,7 @@ udapy -h >/dev/null || { echo "udapy is not installed, see https://github.com/ud
 
 SCENARIO="ud.SplitUnderscoreTokens default_deprel=fixed ud.SetSpaceAfterFromText ud.Convert1to2 skip=nmod"
 IN=ga-ud-all_convert-textline.conllu
-cat $IN | sed '17183s/Loch_Garman\tLoch/Loch_Garman\tLoch_Garman/'| udapy -s $SCENARIO > ga-ud-all.conllu
+cat $IN | sed '17183s/Loch_Garman\tLoch/Loch_Garman\tLoch_Garman/'| udapy -s $SCENARIO util.Eval node='del node.misc["ManualCheck"]' > ga-ud-all.conllu
 
 cat ga-ud-all.conllu | udapy -HAMC ud.MarkBugs skip='no-(VerbForm|PronType|NumType)' > bugs.html
 
